12b630e13b2c2ea1c9b2e86447601b2343dc6dee
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2025-05-14 21:13:58 -0400

    Add prompt model name

--- backend/lib/types.ts
+++ backend/lib/types.ts
@@ -1,32 +1,33 @@
 export type GenerationConfigs = {
-  coloringImagePath: string | null;
-  generateScript: string;
-  generatedOn: string | null;
-  prompt: string;
-  seed: number | null;
-  collectionName: string;
-  tags: string[];
-  modelName: string;
-  name: string;
+    coloringImagePath: string | null;
+    generateScript: string;
+    generatedOn: string | null;
+    prompt: string;
+    seed: number | null;
+    collectionName: string;
+    tags: string[];
+    modelName: string;
+    promptModelName: string;
+    name: string;
 };
 
 export type AppConfigs = {
-  db: {
-    name: string;
-    port: number;
-    user: string;
-    password: string;
-    hostname: string;
-    poolSize: number;
-  };
-  s3: {
-    region: string;
-    accessKeyId: string;
-    secretAccessKey: string;
-  };
-  cloudfrontUrl: string;
-  geminiApiKey: string;
-  comfyServerAddress: string;
-  comfyApiEndpoint: string;
-  comfyOutputDir: string;
+    db: {
+        name: string;
+        port: number;
+        user: string;
+        password: string;
+        hostname: string;
+        poolSize: number;
+    };
+    s3: {
+        region: string;
+        accessKeyId: string;
+        secretAccessKey: string;
+    };
+    cloudfrontUrl: string;
+    geminiApiKey: string;
+    comfyServerAddress: string;
+    comfyApiEndpoint: string;
+    comfyOutputDir: string;
 };
