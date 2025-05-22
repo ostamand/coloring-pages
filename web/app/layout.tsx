ee971bf1508e6c83753650bd738089b9830be067
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2025-05-21 20:51:46 -0400

    Add umami

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,4 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,6 +25,12 @@ export default function RootLayout({
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
                 {children}
+                {/* disable umami tracking: localStorage.setItem("umami.disabled", "true") */}
+                <Script
+                    src="https://analytics.ostamand.com/script.js"
+                    data-website-id="af533c89-10ea-4f06-a17f-ebd190f0a7b9"
+                    strategy="lazyOnload"
+                />
             </body>
         </html>
     );
