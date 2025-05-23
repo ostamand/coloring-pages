574b1b425ffad297676a521f7e03296ef19ab6c2
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2025-05-22 20:14:12 -0400

    Fix contact form

--- web/components/contact-form/contact-form.component.tsx
+++ web/components/contact-form/contact-form.component.tsx
@@ -1,33 +1,123 @@
 "use client";
 
 import styles from "./contact-form.styles.module.scss";
+import stylesButton from "../../styles/button.styles.module.scss";
 
-import { z } from "zod";
+import { useState } from "react";
+import Link from "next/link";
 
-import { Button } from "../ui/button";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
-
-const formSchema = z.object({
-    name: z.string().min(2).max(50),
-    email: z.string().email(),
-    message: z.string(),
-});
+import { Textarea } from "@/components/ui/textarea";
 
 export default function ContactForm() {
+    const defaultErrors = {
+        email: false,
+        name: false,
+        message: false,
+    };
+
+    const [errors, setErrors] = useState(defaultErrors);
+    const [messageSent, setMessageSent] = useState(false);
+
     function submit(formData: FormData) {
-        console.log(formData);
+        const email = formData.get("email");
+        const name = formData.get("name");
+        const message = formData.get("message");
+
+        const errors = { ...defaultErrors };
+
+        // email
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const isEmailValid =
+            typeof email === "string" && emailRegex.test(email);
+        if (!isEmailValid) {
+            errors.email = true;
+        }
+
+        // name
+        if (typeof name !== "string" || name.trim().length === 0) {
+            errors.name = true;
+        }
+
+        // message
+        if (typeof message !== "string" || message.trim().length === 0) {
+            errors.message = true;
+        }
+
+        setErrors(errors);
+
+        if (errors.email || errors.message || errors.name) {
+            return;
+        }
+
+        //TODO send API call
+
+        setMessageSent(true);
     }
 
     return (
         <div className={styles.contactFormContainer}>
-            <div className={styles.contactFormContent}>
-                <form action={submit}>
-                    <Label htmlFor="name">Your Name</Label>
-                    <Input type="text" id="name" />
-                    <Button type="submit">Submit</Button>
-                </form>
-            </div>
+            {!messageSent ? (
+                <div className={styles.contactFormContent}>
+                    <form action={submit}>
+                        <div className={styles.labelPanel}>
+                            <Label htmlFor="name">Your Name</Label>
+                            {errors.name && (
+                                <Label className={styles.errorLabel}>
+                                    Required
+                                </Label>
+                            )}
+                        </div>
+                        <Input id="name" type="text" name="name" />
+                        <div className={styles.labelPanel}>
+                            <Label htmlFor="email">Your Email</Label>
+                            {errors.email && (
+                                <Label className={styles.errorLabel}>
+                                    Not valid
+                                </Label>
+                            )}
+                        </div>
+
+                        <Input id="email" type="email" name="email" />
+
+                        <div className={styles.labelPanel}>
+                            <Label htmlFor="message">Your Message</Label>
+                            {errors.message && (
+                                <Label className={styles.errorLabel}>
+                                    Required
+                                </Label>
+                            )}
+                        </div>
+                        <Textarea id="message" name="message" />
+                        <button
+                            type="submit"
+                            className={`${styles.submitButton} ${stylesButton.actionButton}`}
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            ) : (
+                <div className={styles.messageSentContent}>
+                    <Link href="/">
+                        <img
+                            src="/Logo_ColorItDaily_Smile.png"
+                            alt="Coloring Pages Logo"
+                            height="auto"
+                            width="200px"
+                        />
+                    </Link>
+                    <h2>Thank you!</h2>
+                    <Link href="/">
+                        <div
+                            className={`${styles.homeButton}  ${stylesButton.actionButton} `}
+                        >
+                            Go Back Home
+                        </div>
+                    </Link>
+                </div>
+            )}
         </div>
     );
 }
