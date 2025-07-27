83b105a8cb52e5d11767a83b32920a6c4b7dc287
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2025-07-27 09:37:03 -0400

    Improve layout promo

--- web/components/promo-section/promo-section.component.tsx
+++ web/components/promo-section/promo-section.component.tsx
@@ -8,25 +8,27 @@ import { Promotion } from "@/lib/api/types";
 export default function PromoSection({ promotion }: { promotion: Promotion }) {
     return (
         <div className={styles.promotionContainer}>
-            <div className={styles.promotionHeading}>
-                {promotion.heading_url ? (
-                    <div
-                        className={styles.headingWithImg}
-                        style={{ backgroundColor: promotion.background_color }}
-                    >
-                        <img
-                            src={promotion.heading_url}
-                            alt={`Collection: ${promotion.heading}`}
-                        />
-                    </div>
-                ) : (
-                    <>
-                        <h2>{`Collection: ${promotion.heading}`}</h2>
-                        <h3>{promotion.sub_heading}</h3>
-                    </>
-                )}
-            </div>
             <div className={styles.promotionContent}>
+                <div className={styles.promotionHeading}>
+                    {promotion.heading_url ? (
+                        <div
+                            className={styles.headingWithImg}
+                            style={{
+                                backgroundColor: promotion.background_color,
+                            }}
+                        >
+                            <img
+                                src={promotion.heading_url}
+                                alt={`Collection: ${promotion.heading}`}
+                            />
+                        </div>
+                    ) : (
+                        <>
+                            <h2>{`Collection: ${promotion.heading}`}</h2>
+                            <h3>{promotion.sub_heading}</h3>
+                        </>
+                    )}
+                </div>
                 <div className={styles.backgroundOverlay} />
                 <div className={styles.imageContainer}>
                     <Link href={`/collections/${promotion.collection_name}`}>
