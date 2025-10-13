70544974c1bf72e148be588ba38a42910ebb192c
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2025-10-13 08:36:07 -0400

    Add api count

---
+++ web/app/api/count/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+    try {
+        const url = `${process.env.NEXT_PUBLIC_API_URL}/pages/count`;
+        const response = await fetch(url);
+        if (!response.ok) {
+            return NextResponse.json({
+                status: response.status,
+                error: response.statusText,
+            }, { status: response.status });
+        }
+        const data = await response.json();
+        return NextResponse.json(data);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({
+            status: 500,
+            error: "Internal server error.",
+        }, {
+            status: 500,
+        });
+    }
+}
